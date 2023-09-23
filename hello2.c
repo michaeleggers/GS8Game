7c45a2654deab081f3855ed225262da3de9aa99b
Michael Eggers <mseggers@protonmail.ch>
2023-09-23 22:49:27 +0200

    Add some camera up down movement while walking

--- hello2.c
+++ hello2.c
@@ -1,13 +1,15 @@
 #include <acknex.h>
 #include <default.c>
 
-#define WALK_SPEED        20
+#define WALK_SPEED        1.0
 #define SLOW_WALK_SPEED   WALK_SPEED/2.0
 #define MOUSE_SENSITIVITY 10
 #define ACCELERATION      2.0
 #define MAX_ACCELERATION  6.0
 #define MAX_ROLL_DEG      2.0
 #define ROLL_DEG_INC      0.5
+#define CAM_WAVE_INC      0.5
+#define CAM_WAVE_STRENGTH 5.0
 
 // Define the cube entity
 ENTITY* cube;
@@ -16,6 +18,8 @@ float g_Velocity     = 0.0;
 float g_VelocitySide = 0.0;
 int   g_ForwardDir   = 0;
 float g_RollDeg      = 0.0;
+float g_CamWave      = 0.0;
+float g_SaveCamZ     = 150.0;
 
 function MoveCamera() {
 	VECTOR* newAngle = NULL;
@@ -39,14 +43,20 @@ function MoveCamera() {
 			0);
 		vec_normalize(forward, 1.0);		
 	
+	
 		// Walk forward/backwards
+
 		
 		int forwardDir = 0;
-		if (key_w) {			
-			g_Velocity += time_step*ACCELERATION*ACCELERATION;					
+		if (key_w) {	
+			forwardDir  = 1;		
+			g_Velocity += time_step*ACCELERATION*ACCELERATION;
+			g_CamWave  += time_step*CAM_WAVE_INC;
 		}
 		if (key_s) {
+			forwardDir  = -1;
 			g_Velocity -= time_step*ACCELERATION*ACCELERATION;			
+			g_CamWave  += time_step*CAM_WAVE_INC;
 		}
 		if ( (!key_s && !key_w) || (key_s && key_w)) {
 			if (g_Velocity > 0.0) {
@@ -54,34 +64,53 @@ function MoveCamera() {
 			}
 			if (g_Velocity < 0.0) {
 				g_Velocity += time_step*ACCELERATION;
-			}			
-		}		
+			}
+		
+			// Check where we are at the sine urve. If still > 0 then keep doing until done
+			float sineOfCurrent = sin(g_CamWave);
+			if (sineOfCurrent > 0.0) {
+				g_CamWave += time_step*CAM_WAVE_INC;
+			}
+			else { // Done with waving -> Reset the input value so we start at sin(0) = 0 again.
+				g_CamWave = 0.0;
+			}
+		}
+		//g_CamWave = clamp(g_CamWave, 0.0, 360.0);
+	   
 			
 		float walkSpeed = WALK_SPEED;
 		if (key_shift) {
 			walkSpeed = SLOW_WALK_SPEED;
 		}			
 		g_Velocity = clamp(g_Velocity, -MAX_ACCELERATION, MAX_ACCELERATION);
-		float xCmp = time_step*g_Velocity*walkSpeed*forward.x;
-		float yCmp = time_step*g_Velocity*walkSpeed*forward.y;
+		if (abs(g_Velocity) < 0.001) {
+			g_Velocity = 0;
+		}
+		float xCmp = forward.x;
+		float yCmp = forward.y;
 		newPos = vector(
 			xCmp,
 			yCmp,
 			0
 		);		
+
 				
 		// Straving			
+
 		
+		int sideDir = 0;
 		VECTOR right;
 		VECTOR* up = vector(0, 0, 1);			
 		vec_cross(&right, up, forward);
 		if (key_a) {
-			g_VelocitySide += time_step*ACCELERATION*ACCELERATION;
-			g_RollDeg      -= time_step*ROLL_DEG_INC;
+			g_VelocitySide += time_step*ACCELERATION*ACCELERATION;						
+			sideDir        += 1;
+			g_RollDeg      -= time_step*2*ROLL_DEG_INC;
 		}
 		if (key_d) {
-			g_VelocitySide -= time_step*ACCELERATION*ACCELERATION;	
-			g_RollDeg      += time_step*ROLL_DEG_INC;
+			g_VelocitySide += time_step*ACCELERATION*ACCELERATION;							
+			sideDir        -= 1;
+			g_RollDeg      += time_step*2*ROLL_DEG_INC;
 		}
 		if ( (!key_a && !key_d) || (key_a && key_d) ) {
 			if (g_VelocitySide > 0.0) {
@@ -90,26 +119,28 @@ function MoveCamera() {
 			if (g_RollDeg > 0.0) {
 				g_RollDeg -= time_step*ROLL_DEG_INC;
 			}
-			if (g_VelocitySide < 0.0) {
-				g_VelocitySide += time_step*ACCELERATION;
-			}			
+//			if (g_VelocitySide < 0.0) {
+//				g_VelocitySide += time_step*ACCELERATION;
+//			}			
 			if (g_RollDeg < 0.0) {
 				g_RollDeg += time_step*ROLL_DEG_INC;
 			}
 		}
-		g_VelocitySide = clamp(g_VelocitySide, -MAX_ACCELERATION, MAX_ACCELERATION);
-		if ( abs(g_VelocitySide) < 0.00001 ) {
+		g_VelocitySide = clamp(g_VelocitySide, 0, MAX_ACCELERATION);
+		if ( abs(g_VelocitySide) < 0.001 ) {
 			g_VelocitySide = 0;
 		}
-		g_RollDeg = clamp(g_RollDeg, -MAX_ROLL_DEG, MAX_ROLL_DEG);
-		vec_scale(right, time_step*g_VelocitySide*walkSpeed);
-
-		vec_add(newPos, right);
-		float walkDistance = vec_length(newPos);
-		if (walkDistance > 0.00001) {
-			vec_add(camera.x, newPos);
-		}
 		
+		vec_scale(right, g_VelocitySide*sideDir);
+		vec_scale(newPos, g_Velocity);
+		vec_add(newPos, right);
+		vec_normalize(newPos, walkSpeed);
+		//vec_scale(newPos, walkSpeed);
+		vec_add(camera.x, newPos);
+		float sinAt = abs(sin(g_CamWave));
+		//sinAt = smooth(&sinAt, 0.09);
+		camera.z = g_SaveCamZ + CAM_WAVE_STRENGTH*sinAt;		
+		g_RollDeg = clamp(g_RollDeg, -MAX_ROLL_DEG, MAX_ROLL_DEG);
 		camera.roll = g_RollDeg;
 		//vec_set(camera.x, newPos);
 		
@@ -137,7 +168,7 @@ void main()
 
 	//camera->flags |= SHOW;
 	camera->x = -1500;
-	camera->z = 150;	
+	camera->z = g_SaveCamZ;	
 	    
     // Create the cube
     VECTOR* pos = vector(-100, 0, 150);
